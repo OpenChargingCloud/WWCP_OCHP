01ff97e6bd11ac142011902cbac3570d69d00cc1
Achim Friedland <github@ahzf.de>
2018-01-16 20:46:01 +0100

    Fixed typo

--- WWCP_OCHPv1.4/DataTypes/ParkingSpotInfo.cs
+++ WWCP_OCHPv1.4/DataTypes/ParkingSpotInfo.cs
@@ -419,7 +419,7 @@ namespace org.GraphDefined.WWCP.OCHPv1_4
         #region (override) ToString()
 
         /// <summary>
-        /// Return a string representation of this object.
+        /// Return a text representation of this object.
         /// </summary>
         public override String ToString()
 
