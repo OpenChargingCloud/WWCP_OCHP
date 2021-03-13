4e340707d150b762e1ac3b52bb1da76076c43101
Achim Friedland <achim.friedland@graphdefined.com>
2021-03-13 04:15:39 +0100

    Add CPOPartnerSessionId (OICP) to AuthRequests

--- WWCP_OCHPv1.4_Adapter/CPO/WWCPEMPAdapter.cs
+++ WWCP_OCHPv1.4_Adapter/CPO/WWCPEMPAdapter.cs
@@ -4015,6 +4015,7 @@ namespace org.GraphDefined.WWCP.OCHPv1_4.CPO
         /// <param name="ChargingLocation">The charging location.</param>
         /// <param name="ChargingProduct">An optional charging product.</param>
         /// <param name="SessionId">An optional session identification.</param>
+        /// <param name="CPOPartnerSessionId">An optional session identification of the CPO.</param>
         /// <param name="OperatorId">An optional charging station operator identification.</param>
         /// 
         /// <param name="Timestamp">The optional timestamp of the request.</param>
@@ -4024,15 +4025,16 @@ namespace org.GraphDefined.WWCP.OCHPv1_4.CPO
         public async Task<AuthStartResult>
 
             AuthorizeStart(LocalAuthentication          LocalAuthentication,
-                           ChargingLocation             ChargingLocation    = null,
-                           ChargingProduct              ChargingProduct     = null,   // [maxlength: 100]
-                           ChargingSession_Id?          SessionId           = null,
-                           ChargingStationOperator_Id?  OperatorId          = null,
+                           ChargingLocation             ChargingLocation      = null,
+                           ChargingProduct              ChargingProduct       = null,   // [maxlength: 100]
+                           ChargingSession_Id?          SessionId             = null,
+                           ChargingSession_Id?          CPOPartnerSessionId   = null,
+                           ChargingStationOperator_Id?  OperatorId            = null,
 
-                           DateTime?                    Timestamp           = null,
-                           CancellationToken?           CancellationToken   = null,
-                           EventTracking_Id             EventTrackingId     = null,
-                           TimeSpan?                    RequestTimeout      = null)
+                           DateTime?                    Timestamp             = null,
+                           CancellationToken?           CancellationToken     = null,
+                           EventTracking_Id             EventTrackingId       = null,
+                           TimeSpan?                    RequestTimeout        = null)
 
         {
 
@@ -4076,6 +4078,7 @@ namespace org.GraphDefined.WWCP.OCHPv1_4.CPO
                                                 ChargingLocation,
                                                 ChargingProduct,
                                                 SessionId,
+                                                CPOPartnerSessionId,
                                                 new ISendAuthorizeStartStop[0],
                                                 RequestTimeout);
 
@@ -4212,6 +4215,7 @@ namespace org.GraphDefined.WWCP.OCHPv1_4.CPO
                                                  ChargingLocation,
                                                  ChargingProduct,
                                                  SessionId,
+                                                 CPOPartnerSessionId,
                                                  new ISendAuthorizeStartStop[0],
                                                  RequestTimeout,
                                                  result,
@@ -4239,6 +4243,7 @@ namespace org.GraphDefined.WWCP.OCHPv1_4.CPO
         /// <param name="SessionId">The session identification from the AuthorizeStart request.</param>
         /// <param name="LocalAuthentication">An user identification.</param>
         /// <param name="ChargingLocation">The charging location.</param>
+        /// <param name="CPOPartnerSessionId">An optional session identification of the CPO.</param>
         /// <param name="OperatorId">An optional charging station operator identification.</param>
         /// 
         /// <param name="Timestamp">The optional timestamp of the request.</param>
@@ -4249,13 +4254,14 @@ namespace org.GraphDefined.WWCP.OCHPv1_4.CPO
 
             AuthorizeStop(ChargingSession_Id           SessionId,
                           LocalAuthentication          LocalAuthentication,
-                          ChargingLocation             ChargingLocation    = null,
-                          ChargingStationOperator_Id?  OperatorId          = null,
+                          ChargingLocation             ChargingLocation      = null,
+                          ChargingSession_Id?          CPOPartnerSessionId   = null,
+                          ChargingStationOperator_Id?  OperatorId            = null,
 
-                          DateTime?                    Timestamp           = null,
-                          CancellationToken?           CancellationToken   = null,
-                          EventTracking_Id             EventTrackingId     = null,
-                          TimeSpan?                    RequestTimeout      = null)
+                          DateTime?                    Timestamp             = null,
+                          CancellationToken?           CancellationToken     = null,
+                          EventTracking_Id             EventTrackingId       = null,
+                          TimeSpan?                    RequestTimeout        = null)
         {
 
             #region Initial checks
@@ -4296,6 +4302,7 @@ namespace org.GraphDefined.WWCP.OCHPv1_4.CPO
                                                    OperatorId,
                                                    ChargingLocation,
                                                    SessionId,
+                                                   CPOPartnerSessionId,
                                                    LocalAuthentication,
                                                    RequestTimeout);
 
@@ -4378,6 +4385,7 @@ namespace org.GraphDefined.WWCP.OCHPv1_4.CPO
                                                     OperatorId,
                                                     ChargingLocation,
                                                     SessionId,
+                                                    CPOPartnerSessionId,
                                                     LocalAuthentication,
                                                     RequestTimeout,
                                                     result,
